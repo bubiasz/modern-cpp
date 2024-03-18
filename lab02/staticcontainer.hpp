165b18d06e934a061f7d5796149bdced48f122af
Bubiasz <jakub.matysek@outlook.com>
2024-03-18 17:33:33 +0100

    Done staticcontainer

---
+++ lab02/staticcontainer.hpp
@@ -0,0 +1,71 @@
+//
+//  staticcontainer.hpp
+//  lab02
+//
+//  Created by Jakub Matysek on 05/03/2024.
+//
+
+#ifndef staticcontainer_hpp
+#define staticcontainer_hpp
+
+#include "box.hpp"
+#include <iostream>
+#include <utility>
+
+class Container : public Box {
+public:
+    static bool verbose;
+
+    Container(int content)
+        : Box(content) { }
+
+    Container(const Container& container)
+        : Box(container) {
+        if (verbose)
+            std::cout << "Container: copy constructor.\n";
+    }
+
+    Container& operator=(const Container& container) {
+        if (verbose)
+            std::cout << "Container: copy assignment.\n";
+        if (this != &container) {
+            Box::operator=(container);
+        }
+        return *this;
+    }
+
+    Container(Container&& container)
+        : Box(std::move(container)) {
+        if (verbose)
+            std::cout << "Container: move constructor.\n";
+    }
+
+    Container& operator=(Container&& container) {
+        if (verbose)
+            std::cout << "Container: move assignment.\n";
+        if (this != &container) {
+            Box::operator=(std::move(container));
+        }
+        return *this;
+    }
+
+    ~Container() {
+        if (verbose)
+            std::cout << "Container destructor. \n";
+    }
+
+    friend Container operator+(const Container& p1, const Container& p2);
+
+    friend std::ostream& operator<<(std::ostream& out, const Container& p) {
+        return (std::cout << " [" << p.getContent() << "] ");
+    }
+};
+
+bool Container::verbose = false;
+
+inline Container operator+(const Container& p1, const Container& p2) {
+    Container suma(p1.getContent() + p2.getContent());
+    return suma;
+}
+
+#endif /* staticcontainer_hpp */
